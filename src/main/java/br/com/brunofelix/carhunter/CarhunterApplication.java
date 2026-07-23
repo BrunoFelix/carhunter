f6cee5e3ae67ccda163560096ee44a1513b77a69
Bruno Felix <brunofelixmbarbosa@gmail.com>
2026-07-23 11:53:25 -0300

    init

---
+++ src/main/java/br/com/brunofelix/carhunter/CarhunterApplication.java
@@ -0,0 +1,13 @@
+package br.com.brunofelix.carhunter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarhunterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarhunterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/brunofelix/carhunter/CarhunterApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.brunofelix.carhunter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarhunterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
